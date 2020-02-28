2be7e3279b8e55b8b25a40d580aa4e85160baaca
LaGvidilo <cyrilcoelho67@gmail.com>
2020-02-28 06:22:22 +0100

    preparation preliminaire pour exploitation

--- tic_tac_taco_pizza.py
+++ tic_tac_taco_pizza.py
@@ -1,4 +1,4 @@
-"""Play tic tac toe with taco 🌮 and pizza 🍕"""
+"""Play tic tac toe with taco  and pizza """
 
 import click
 import sys
@@ -31,42 +31,42 @@ class TicTacTacoPizza:
 
     @property
     def player_is_pizza(self):
-        """Return True if player's token is 🍕"""
-        return self.token in ('pizza', '🍕')
+        """Return True if player's token is """
+        return self.token in ('pizza', '')
 
     @property
     def player_is_taco(self):
-        """Return True if player's token is 🌮"""
-        return self.token in ('taco', '🌮')
+        """Return True if player's token is """
+        return self.token in ('taco', '')
 
     @property
     def player_token(self):
         """
-        Returns the player's token, either 🍕 or 🌮
+        Returns the player's token, either  or 
         """
         if self.player_is_pizza:
-            return '🍕'
+            return ''
         else:
-            return '🌮'
+            return ''
 
     @property
     def bot_token(self):
         if self.player_is_pizza:
-            return '🌮'
+            return ''
         else:
-            return '🍕'
+            return ''
 
     def print_board(self):
         """
         Print the board
         """
-        click.echo(textwrap.dedent(f"""
-         {self.get_slot(0)} | {self.get_slot(1)} | {self.get_slot(2)}
-        ---+---+---
-         {self.get_slot(3)} | {self.get_slot(4)} | {self.get_slot(5)}
-        ---+---+---
-         {self.get_slot(6)} | {self.get_slot(7)} | {self.get_slot(8)}
-        """))
+        click.echo(textwrap.dedent(
+        str(self.get_slot(0))+""" | """+str(self.get_slot(1))+""" | """+str(self.get_slot(2))+
+        """\n---+---+---\n"""+
+         str(self.get_slot(3))+""" | """+str(self.get_slot(4))+""" | """+str(self.get_slot(5))+
+        """\n---+---+---\n"""+
+         str(self.get_slot(6))+""" | """+str(self.get_slot(7))+""" | """+str(self.get_slot(8))+"\n"
+         ))
 
     def play(self):
         """Main Game loop"""
@@ -87,19 +87,19 @@ class TicTacTacoPizza:
         finished = self.game_is_finished()
         if finished:
             if not self.player_wins:
-                click.echo(f"🤖  won this game. {self.bot_token*5}")
+                click.echo("  won this game. "+str(self.bot_token*5))
             else:
-                click.echo(f"Congratulations 😀  you won!!! {self.player_token*5}")
+                click.echo("Congratulations   you won!!! "+str(self.player_token*5))
 
         else:
-            click.echo("🤷🏻‍♀️  It ends with a tie 👔  😅  ")
-            click.echo("Thanks for playing! 👍")
+            click.echo("  It ends with a tie  ")
+            click.echo("Thanks for playing!")
 
     def bot_move(self):
         """Let the bot make a move, return the last move"""
         available_moves = self.get_available_moves()
         next_move = random.choice(available_moves)
-        click.echo(f"🤖   made a move to {next_move}")
+        click.echo("   made a move to "+str(next_move))
         self.make_move(self.bot_token, next_move)
         return next_move
 
@@ -110,11 +110,11 @@ class TicTacTacoPizza:
         """
         valid_move = False
         while valid_move is False:
-            move = click.prompt(f"It's your turn 😀 ! Move your {self.player_token}  to which position? {self.get_available_moves()}", type=int)
+            move = click.prompt("It's your turn  ! Move your "+str(self.player_token)+"  to which position? "+str(self.get_available_moves()), type=int)
             if self.is_valid_move(move):
                 valid_move = move
             else:
-                click.echo("😔  That's not a valid move.")
+                click.echo("That's not a valid move.")
         return valid_move
 
     def make_move(self, token, index):
@@ -153,7 +153,7 @@ class TicTacTacoPizza:
                       self.board[pattern[1]], \
                       self.board[pattern[2]]
 
-            if a == b == c and a in ('🌮', '🍕'):
+            if a == b == c and a in ('', ''):
                 self.has_winner = True
                 if a == self.player_token:
                     self.player_wins = True
@@ -162,15 +162,15 @@ class TicTacTacoPizza:
 
 
     def rock_paper_scissor(self):
-        who_start = random.choice(['🌮', '🍕'])
-        click.echo("👊  📃  ✂️")
+        who_start = random.choice(['', ''])
+        click.echo("ROCK/PAPER/SCISSORS")
         if self.player_token == who_start:
-            next_player = "😀"
+            next_player = ""
         else:
-            next_player = "🤖"
+            next_player = ""
         click.echo(
-            textwrap.dedent(f"""
-            By a virtual rock paper scissors game, it was determined that {next_player}  should move their {who_start} !
+            textwrap.dedent("""
+            By a virtual rock paper scissors game, it was determined that """+str(next_player)+"""  should move their """+str(who_start)+""" !
             """))
         return who_start
 
@@ -183,41 +183,41 @@ class TicTacTacoPizza:
 
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.version_option(version=__version__)
-@click.option('--token', help='Your choice of token: pizza, taco, 🌮 , or  🍕   ')
+@click.option('--token', help='Your choice of token: pizza, taco,  , or     ')
 def tic_tac_taco_pizza(token):
 
-    click.echo("Welcome to tic tac taco pizza 🐍  🌮  🍕")
+    click.echo("Welcome to tic tac taco pizza    ")
     click.echo()
     if not token:
         token = get_token()
     tic_tac_taco_pizza = TicTacTacoPizza(token)
-    click.echo(f"You 😀  : {tic_tac_taco_pizza.player_token}")
+    click.echo("You   : "+str(tic_tac_taco_pizza.player_token))
     click.echo("vs")
-    click.echo(f"bot 🤖  : {tic_tac_taco_pizza.bot_token}")
+    click.echo("bot   : "+str(tic_tac_taco_pizza.bot_token))
     click.echo()
     tic_tac_taco_pizza.play()
 
 def get_token():
     """
     Prompt user for the token
-    Return the token, either 🌮 or 🍕
+    Return the token, either  or 
     :return:
     """
     token = None
 
     while not token:
         value = click.prompt(textwrap.dedent("""
-        Please choose one of: pizza, taco, 🌮 , or  🍕 . (exit to cancel)
+        Please choose one of: pizza, taco,  , or   . (exit to cancel)
         """))
         value_lower = value.strip().lower()
-        if value_lower in ('pizza', '🍕'):
-            return '🍕'
-        elif value_lower in ('taco', '🌮'):
-            return '🌮'
+        if value_lower in ('pizza', ''):
+            return ''
+        elif value_lower in ('taco', ''):
+            return ''
         elif value_lower == 'exit':
             sys.exit()
         else:
-            click.echo("😔  Invalid choice...")
+            click.echo("Invalid choice...")
 
 
 
